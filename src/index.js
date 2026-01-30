22ed085f974a7b65b395588481858c9f0b94f150
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2026-01-30 13:19:34 +0800

    feat: add test gameboard to render

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import { Ship, Gameboard, Player } from "./model.js";
+import { renderGameboard } from "./view.js";
 import "./styles.css";
+
+let testState = [
+	[1, 1, 1, -1, 2],
+	[2, 2, 2, -2, 2],
+	[2, 2, 2, 2, 2],
+	[2, 1, 2, -2, 2],
+	[2, 1, 2, 2, 2],
+];
+
+const player1GameboardContainer = document.querySelector("#player-one-board");
+
+renderGameboard(testState, player1GameboardContainer);
