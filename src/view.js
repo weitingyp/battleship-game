b6a04460ec826ef65b522e2a00fcc9314e52e891
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2026-01-30 13:13:49 +0800

    feat: renderGameboard view function which appends gameboard to containerNode
    - renderGameboard function takes a gameboardArray and appends it to a containerNode
    - styling for unknown, empty and sunken cells added
    --- unknown cells in light blue
    --- empty cells marked with circle and yellow background
    --- sunken cells marked with red cross and red border

---
+++ src/view.js
@@ -0,0 +1,34 @@
+function renderGameboard(gameboardArray, container) {
+	// given a 2D array, render the gameboard
+	let gameboard = document.createElement("div");
+	gameboard.className = "gameboard";
+	for (let i = 0; i < gameboardArray.length; i++) {
+		let row = document.createElement("div");
+		row.className = "gameboard-row";
+		for (let j = 0; j < gameboardArray.length; j++) {
+			let cell = document.createElement("div");
+			cell.classList.add("cell");
+			if (gameboardArray[i][j] > 0) cell.classList.add("unknown-cell");
+			else if (gameboardArray[i][j] === -2) markEmptyCell(cell);
+			else if (gameboardArray[i][j] === -1) markSunkenCell(cell);
+			row.appendChild(cell);
+		}
+		gameboard.appendChild(row);
+	}
+	container.appendChild(gameboard);
+}
+
+function markEmptyCell(cellNode) {
+	cellNode.classList.remove("unknown-cell");
+	cellNode.classList.add("empty-cell");
+	const circle = document.createElement("div");
+	circle.className = "empty-circle";
+	cellNode.appendChild(circle);
+}
+
+function markSunkenCell(cellNode) {
+	cellNode.classList.remove("unknown-cell");
+	cellNode.classList.add("sunken-cell");
+}
+
+export { renderGameboard };
